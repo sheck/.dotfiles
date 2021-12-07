94666ab4b6b84b597ccb2b8a35d58498644c2797
Nick Sheck <sheck@users.noreply.github.com>
2021-12-07 08:53:04 -0800

    env.sh does this already

--- zsh/configs/pco.zsh
+++ zsh/configs/pco.zsh
@@ -1,8 +1,3 @@
 [ -s ~/Code/pco/bin/pco ] && eval "$(~/Code/pco/bin/pco init -)"
 
-export RBENV_ROOT=$HOME/.rbenv
-export MYSQL_PORT_3306_TCP_ADDR=127.0.0.1
-export MYSQL_READER_PORT_3306_TCP_ADDR=127.0.0.1
-export MYSQL_READER_PORT_3306_TCP_PORT=3307
-
 source $HOME/pco-box/env.sh

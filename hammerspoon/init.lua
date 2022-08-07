95abdf7d5ad9dc6dbd8303ee33c527ecf1758d83
Nick Sheck <sheck@users.noreply.github.com>
2022-08-07 15:41:51 -0700

    autolaunch improvements
    - only on launch, not on focus
    - launch dash when iTerm is opened

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,11 +1,21 @@
 hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 
-function applicationWatcher(appName, eventType, appObject)
-  if (eventType == hs.application.watcher.activated) then
+function krispLauncher(appName, eventType, appObject)
+  if (eventType == hs.application.watcher.launched) then
     if (appName == "zoom.us") then
       hs.application.open("krisp")
     end
   end
 end
-appWatcher = hs.application.watcher.new(applicationWatcher)
-appWatcher:start()
+klauncher = hs.application.watcher.new(krispLauncher)
+klauncher:start()
+
+function dashLauncher(appName, eventType, appObject)
+  if (eventType == hs.application.watcher.launched) then
+    if (appName == "iTerm2") then
+      hs.application.open("Dash")
+    end
+  end
+end
+dlauncher = hs.application.watcher.new(dashLauncher)
+dlauncher:start()

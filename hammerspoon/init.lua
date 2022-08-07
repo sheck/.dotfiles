b26a55bc0485e0e765e0fc73a829399fd7697ea2
Nick Sheck <sheck@users.noreply.github.com>
2022-08-07 12:54:27 -0700

    open krisp when zoom opens

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,1 +1,11 @@
 hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+
+function applicationWatcher(appName, eventType, appObject)
+  if (eventType == hs.application.watcher.activated) then
+    if (appName == "zoom.us") then
+      hs.application.open("krisp")
+    end
+  end
+end
+appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()

2e80095e026513a2ae6db40d1931d079a33b6b36
Nick Sheck <sheck@users.noreply.github.com>
2022-08-23 10:24:41 -0700

    Update env.zsh

--- zsh/configs/env.zsh
+++ zsh/configs/env.zsh
@@ -1,7 +1,8 @@
 if [[ `uname` == "Darwin" ]]; then
   # macOS
   export THOR_MERGE=mergetool-for-rails
+  # New homebrew thing
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 elif command apt > /dev/null; then
   # debian
 fi
-

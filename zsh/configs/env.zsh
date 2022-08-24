9fc40fa7787528d34f4859e8d2a710ceead3d03b
Nick Sheck <sheck@users.noreply.github.com>
2022-08-24 09:59:32 -0700

    Only do new homebrew thing for the new installs

--- zsh/configs/env.zsh
+++ zsh/configs/env.zsh
@@ -1,8 +1,11 @@
 if [[ `uname` == "Darwin" ]]; then
   # macOS
   export THOR_MERGE=mergetool-for-rails
+
   # New homebrew thing
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  if [[ -f "/opt/homebrew/bin/brew" ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
 elif command apt > /dev/null; then
   # debian
 fi

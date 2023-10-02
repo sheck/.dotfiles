8ee0a16cc9cbcf8db56a216369577e26dedf8718
Nick Sheck <sheck@users.noreply.github.com>
2023-10-02 15:34:20 -0700

    Fix transparency

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
-return {
-  -- add gruvbox
-  { "srcery-colors/srcery-vim" },
+-- defer to iterm2 transparency
+vim.g.srcery_bg_passthrough = 1
 
-  -- Configure LazyVim to load gruvbox
+return {
+  -- add srcery, use sha from before transparency was broken for terminal: https://github.com/srcery-colors/srcery-vim/pull/104
+  { "srcery-colors/srcery-vim", commit = "755e4ab8a63ec2b4ea756a2fa128d642771369e5" },
+
+  -- Configure LazyVim to load srcery
   {
     "LazyVim/LazyVim",
     opts = {

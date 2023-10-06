24e3a2b5e7299ebf80d5a14df238a5bebc2c0487
Nick Sheck <sheck@users.noreply.github.com>
2023-10-06 15:32:55 -0700

    fix textwidth in general and for git commits

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Use 72 (instead of 80) for the wrap point for commits
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "gitcommit" },
+  callback = function()
+    vim.opt_local.textwidth = 72
+  end,
+})

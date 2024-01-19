4c2a0978df33f9007d6874740ea91fd304dcf5ec
Nick Sheck <sheck@users.noreply.github.com>
2024-01-19 09:58:17 -0800

    disable dash and krisp launchers

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,21 +1,21 @@
 hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 
-function krispLauncher(appName, eventType, appObject)
-  if (eventType == hs.application.watcher.launched) then
-    if (appName == "zoom.us") then
-      hs.application.open("krisp")
-    end
-  end
-end
-klauncher = hs.application.watcher.new(krispLauncher)
-klauncher:start()
-
-function dashLauncher(appName, eventType, appObject)
-  if (eventType == hs.application.watcher.launched) then
-    if (appName == "iTerm2") then
-      hs.application.open("Dash")
-    end
-  end
-end
-dlauncher = hs.application.watcher.new(dashLauncher)
-dlauncher:start()
+-- function krispLauncher(appName, eventType, appObject)
+--   if (eventType == hs.application.watcher.launched) then
+--     if (appName == "zoom.us") then
+--       hs.application.open("krisp")
+--     end
+--   end
+-- end
+-- klauncher = hs.application.watcher.new(krispLauncher)
+-- klauncher:start()
+--
+-- function dashLauncher(appName, eventType, appObject)
+--   if (eventType == hs.application.watcher.launched) then
+--     if (appName == "iTerm2") then
+--       hs.application.open("Dash")
+--     end
+--   end
+-- end
+-- dlauncher = hs.application.watcher.new(dashLauncher)
+-- dlauncher:start()

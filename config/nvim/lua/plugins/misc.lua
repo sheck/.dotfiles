8c019fcd58a26844eef8a7ec76f082227977077b
Nick Sheck <sheck@users.noreply.github.com>
2024-05-20 15:03:38 -0700

    copilot

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -36,6 +36,29 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
 
   {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      -- debug = true, -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {

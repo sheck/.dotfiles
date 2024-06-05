b3c4bdeb62f0cbfdafebb6454ae11937d22d53ec
Nick Sheck <sheck@users.noreply.github.com>
2024-06-05 10:05:21 -0700

    Remove duplicate typescript declaration

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -33,7 +33,6 @@ return {
 
   -- { import = "lazyvim.plugins.extras.test.core" },
   -- { import = "lazyvim.plugins.extras.dap.core" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
 
   {

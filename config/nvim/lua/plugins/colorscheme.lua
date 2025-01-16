3c6c954743a6899e82b053629667cd659cdf044b
Nick Sheck <sheck@users.noreply.github.com>
2025-01-16 09:56:56 -0800

    Update colorscheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,8 @@
 -- defer to iterm2 transparency
-vim.g.srcery_bg_passthrough = 1
+vim.g.srcery_bg = { "NONE", "NONE" }
 
 return {
-  -- add srcery, use sha from before transparency was broken for terminal: https://github.com/srcery-colors/srcery-vim/pull/104
-  { "srcery-colors/srcery-vim", commit = "755e4ab8a63ec2b4ea756a2fa128d642771369e5" },
+  { "srcery-colors/srcery-vim" },
 
   {
     "rcarriga/nvim-notify",

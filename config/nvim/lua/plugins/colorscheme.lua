3545324c10a6042ecc1f03acc55d33eeb4c998d3
Nick Sheck <sheck@users.noreply.github.com>
2025-05-06 09:27:37 -0700

    Revert "Gruvbox"
    This reverts commit 2e7a18df97b9ee8592379424d479f4f29a00dac8.

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,20 @@
--- -- defer to iterm2 transparency
--- vim.g.srcery_bg = { "NONE", "NONE" }
---
--- return {
---   { "srcery-colors/srcery-vim" },
---
---   {
---     "rcarriga/nvim-notify",
---     -- Remove warning notification on startup from transparent bg
---     opts = { background_colour = "#000000" },
---   },
---
---   -- Configure LazyVim to load srcery
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "srcery",
---     },
---   },
--- }
+-- defer to iterm2 transparency
+vim.g.srcery_bg = { "NONE", "NONE" }
 
 return {
-  { "ellisonleao/gruvbox.nvim", opts = { transparent_mode = true } },
+  { "srcery-colors/srcery-vim" },
 
+  {
+    "rcarriga/nvim-notify",
+    -- Remove warning notification on startup from transparent bg
+    opts = { background_colour = "#000000" },
+  },
+
+  -- Configure LazyVim to load srcery
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "srcery",
     },
   },
 }

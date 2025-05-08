b100070379aa19d49d21161a14f3ac4809795ec5
Nick Sheck <sheck@users.noreply.github.com>
2025-05-08 10:26:18 -0700

    fix mason issue

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -6,6 +6,10 @@ vim.g.autoformat = false
 -- vim.g["test#preserve_screen"] = 0
 
 return {
+  -- https://github.com/LazyVim/LazyVim/issues/6039#issuecomment-2856227817
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+
   {
     "Wansmer/treesj",
     keys = {

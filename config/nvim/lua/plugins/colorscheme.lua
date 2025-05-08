d94b09956c6e28035f12d3ac8d6cbee7d4635851
Nick Sheck <sheck@users.noreply.github.com>
2025-05-08 09:13:28 -0700

    cleanup

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -4,12 +4,6 @@ vim.g.srcery_bg = { "NONE", "NONE" }
 return {
   { "srcery-colors/srcery-vim" },
 
-  {
-    "rcarriga/nvim-notify",
-    -- Remove warning notification on startup from transparent bg
-    opts = { background_colour = "#000000" },
-  },
-
   -- Configure LazyVim to load srcery
   {
     "LazyVim/LazyVim",

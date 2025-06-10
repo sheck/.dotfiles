a4fae19c9453ebb1c55cb13d51362b989d3b18c7
Nick Sheck <sheck@users.noreply.github.com>
2025-06-10 09:01:44 -0700

    claud code plugin

--- config/nvim/lua/plugins/misc.lua
+++ config/nvim/lua/plugins/misc.lua
@@ -11,6 +11,14 @@ return {
   { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 
   {
+    "greggh/claude-code.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("claude-code").setup()
+    end,
+  },
+
+  {
     "Wansmer/treesj",
     keys = {
       {

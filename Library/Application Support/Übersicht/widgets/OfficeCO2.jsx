eed11d5cc746670312cb80dd6dea7455f3da635e
Nick Sheck <sheck@users.noreply.github.com>
2025-08-05 11:42:07 -0700

    mkrc ~/Library/Application\ Support/Übersicht/widgets/OfficeCO2.jsx

---
+++ Library/Application Support/Übersicht/widgets/OfficeCO2.jsx
@@ -0,0 +1,17 @@
+const seconds = 10
+export const refreshFrequency = seconds * 1000
+
+export const command = 'shortcuts run "Office CO2 Level" | xargs'
+
+export const className = `
+  font-family: system-ui;
+  margin: 1%;
+  opacity: .5;
+  @media (prefers-color-scheme: dark) {
+    color: #fff;
+  }
+`
+
+export function render({ output }) {
+  return <h4>{output}</h4>
+}
